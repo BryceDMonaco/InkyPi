300a1525328799d5773a77f23deeee2109c3852d
Bryce Monaco <brycedmonaco@gmail.com>
2025-09-10 20:01:39 -0700

    Initial creation of surfer plugin

---
+++ src/plugins/surfer/surfer.py
@@ -0,0 +1,87 @@
+from plugins.base_plugin.base_plugin import BasePlugin
+
+
+class Surfer(BasePlugin):
+    def generate_settings_template(self):
+        template_params = super().generate_settings_template()
+        template_params['api_key'] = {
+            "required": True,
+            "service": "OpenWeatherMap",
+            "expected_key": "OPEN_WEATHER_MAP_SECRET"
+        }
+        template_params['style_settings'] = True
+        return template_params
+
+    def generate_image(self, settings, device_config):
+        lat = settings.get('latitude')
+        long = settings.get('longitude')
+        if not lat or not long:
+            raise RuntimeError("Latitude and Longitude are required.")
+
+        weather_provider = settings.get('weatherProvider', 'OpenWeatherMap')
+        title = settings.get('customTitle', '')
+
+        timezone = device_config.get_config("timezone", default="America/New_York")
+        time_format = device_config.get_config("time_format", default="12h")
+        tz = pytz.timezone(timezone)
+
+        # Gather and parse weather data
+        try:
+            if weather_provider == "OpenWeatherMap":
+                api_key = device_config.load_env_key("OPEN_WEATHER_MAP_SECRET")
+                if not api_key:
+                    raise RuntimeError("Open Weather Map API Key not configured.")
+                weather_data = self.get_weather_data(api_key, units, lat, long)
+                if settings.get('titleSelection', 'location') == 'location':
+                    title = self.get_location(api_key, lat, long)
+                template_params = self.parse_weather_data(weather_data, aqi_data, tz, units, time_format)
+            else:
+                raise RuntimeError(f"Unknown weather provider: {weather_provider}")
+
+            template_params['title'] = title
+        except Exception as e:
+            logger.error(f"{weather_provider} request failed: {str(e)}")
+            raise RuntimeError(f"{weather_provider} request failure, please check logs.")
+
+        # Gather and parse surf data
+
+        # Have language model summarize
+
+        dimensions = device_config.get_resolution()
+        if device_config.get_config("orientation") == "vertical":
+            dimensions = dimensions[::-1]
+
+        template_params["plugin_settings"] = settings
+
+        # Add last refresh time
+        now = datetime.now(tz)
+        if time_format == "24h":
+            last_refresh_time = now.strftime("%Y-%m-%d %H:%M")
+        else:
+            last_refresh_time = now.strftime("%Y-%m-%d %I:%M %p")
+        template_params["last_refresh_time"] = last_refresh_time
+
+        image = self.render_image(dimensions, "weather.html", "weather.css", template_params)
+
+        if not image:
+            raise RuntimeError("Failed to take screenshot, please check logs.")
+        return image
+
+    def parse_weather_data(self, weather_data, aqi_data, tz, units, time_format):
+        current = weather_data.get("current")
+        dt = datetime.fromtimestamp(current.get('dt'), tz=timezone.utc).astimezone(tz)
+        current_icon = current.get("weather")[0].get("icon").replace("n", "d")
+        data = {
+            "current_date": dt.strftime("%A, %B %d"),
+            "current_day_icon": self.get_plugin_dir(f'icons/{current_icon}.png'),
+            "current_temperature": str(round(current.get("temp"))),
+            "feels_like": str(round(current.get("feels_like"))),
+            "temperature_unit": UNITS[units]["temperature"],
+            "units": units,
+            "time_format": time_format
+        }
+        data['forecast'] = self.parse_forecast(weather_data.get('daily'), tz)
+        data['data_points'] = self.parse_data_points(weather_data, aqi_data, tz, units, time_format)
+
+        data['hourly_forecast'] = self.parse_hourly(weather_data.get('hourly'), tz, time_format)
+        return data
