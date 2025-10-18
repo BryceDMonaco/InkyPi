ec917e0b4d1f4820efcbdfae1c90fd08ac06e6da
Bryce Monaco <brycedmonaco@gmail.com>
2025-10-18 13:20:57 -0700

    Added support for time conversion

--- src/plugins/surfer/surfer.py
+++ src/plugins/surfer/surfer.py
@@ -64,7 +64,10 @@ class Surfer(BasePlugin):
             parsed_tide_data = self.parse_surf_data(raw_tide_data)
 
             # Format times once since they are used in multiple places
-            formatted_times = [t.strftime("%H:00") for t in parsed_weather_data['time'].tolist()]
+            if time_format == "24h":
+                formatted_times = [t.strftime("%H:00") for t in parsed_weather_data['time'].tolist()]
+            else:
+                formatted_times = [t.strftime("%I:00 %p") for t in parsed_weather_data['time'].tolist()]
 
             # Convert wind direction (0-360 deg) to compass directions
             parsed_weather_data['windDirectionCompass'] = parsed_weather_data['windDirection'].apply(self.degrees_to_compass)
